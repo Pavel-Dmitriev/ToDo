7a9d747c087abfc7293e643b5debb2de5cc76c7d
Pavel Dmitriev <pdmitriev@fil-it.ru>
2022-08-28 12:26:22 +0300

    Добавлено обновление localStorage при изменении состояния дела - done, isOpen

--- src/api/localStorage.ts
+++ src/api/localStorage.ts
@@ -2,7 +2,7 @@ import { ITodoItem } from "components/layouts/TodoList/interface";
 
 export const localStorageKey = "Список дел";
 
-export function getLocalStorageTodos(): ITodoItem[] | void {
+export function getLocalStorageTodos(): ITodoItem[] | undefined {
   const source = localStorage.getItem(localStorageKey);
   if (source) {
     return JSON.parse(source);
@@ -13,3 +13,20 @@ export function getLocalStorageTodos(): ITodoItem[] | void {
 export function setLocalStorageTodos(todos: ITodoItem[]) {
   localStorage.setItem(localStorageKey, JSON.stringify(todos));
 }
+
+// export const todoGetFx = createEffect<void, ITodoItem[], Error>(() => {
+//   const history = getLocalStorageTodos();
+//   return history ?? [];
+// });
+
+// export const todoSetFx = createEffect(({ text }: ITodoItem) => {
+//   const todo = {
+//     id: Math.random().toString(36).substring(2, 9),
+//     text,
+//     done: false,
+//     isOpen: false,
+//   };
+//   const history = getLocalStorageTodos();
+//   setLocalStorageTodos([...(history as ITodoItem[]), todo]);
+//   return todo;
+// });
--- src/components/layouts/TodoList/index.tsx
+++ src/components/layouts/TodoList/index.tsx
@@ -4,8 +4,7 @@ import { useStore } from "effector-react";
 import TodoItem from "components/layouts/TodoItem";
 
 import { $todoList, getTodos, openTodoDetails, toggleTodo } from "./store";
-
-// addTodo.watch((data) => console.log(data));
+import { setLocalStorageTodos } from "api/localStorage";
 
 function TodoList({ setOpen, setId }: any) {
   const items = useStore($todoList);
@@ -18,17 +17,25 @@ function TodoList({ setOpen, setId }: any) {
     <ul>
       {items?.map((item) => {
         const { id, text, done, isOpen } = item;
+
+        const toggleTodoItem = () => {
+          toggleTodo(item);
+        };
+
+        const toggleTodoDetails = () => {
+          setId(id);
+          openTodoDetails(item);
+        };
+
+        setLocalStorageTodos(items);
         return (
           <TodoItem
             done={done}
             text={text}
             key={`todo_item_${id}`}
             id={id}
-            toggle={() => toggleTodo(item)}
-            toggleTodoDetails={() => {
-              setId(id);
-              openTodoDetails(item);
-            }}
+            toggle={toggleTodoItem}
+            toggleTodoDetails={toggleTodoDetails}
             isOpen={isOpen}
             setOpen={setOpen}
           />
