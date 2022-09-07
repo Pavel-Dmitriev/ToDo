9d66bc3c2acc06280213eeac2e426bf184fd58dc
Pavel Dmitriev <pdmitriev@fil-it.ru>
2022-09-07 17:40:39 +0300

    Добавлен компонент NoData

---
+++ src/components/uikit/NoData/index.tsx
@@ -0,0 +1,9 @@
+function NoData() {
+  return (
+    <div className="flex min-h-200 items-center justify-center rounded-default bg-white text-xl font-bold text-blue">
+      Список дел пуст!
+    </div>
+  );
+}
+
+export default NoData;
