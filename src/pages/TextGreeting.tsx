da0834e73a938ce812ae5df252cf1283837df639
Pavel Dmitriev <pdmitriev@fil-it.ru>
2022-11-17 12:50:10 +0300

    Отобразить для приветственной страницы количество активных дел.

--- src/pages/TextGreeting.tsx
+++ src/pages/TextGreeting.tsx
@@ -4,8 +4,8 @@ import { useStore } from "effector-react";
 import { $todoList, getTodos } from "pages/TodoPage/components/TodoList/store";
 
 function TextGreeting() {
-  // TODO вынести todos в пропсы.
   const todos = useStore($todoList);
+  const countActiveTodos = todos?.filter((todo) => !todo.done).length;
 
   useEffect(() => {
     getTodos();
@@ -14,7 +14,7 @@ function TextGreeting() {
   return (
     <section className="flex grow flex-col items-center justify-center text-blue">
       <h1 className="text-2xl">Приложение список дел</h1>
-      {!!todos?.length && <p>Кол-во ваших активных дел: {todos?.length}</p>}
+      {countActiveTodos > 0 && <p>Кол-во ваших активных дел: {countActiveTodos}</p>}
     </section>
   );
 }
